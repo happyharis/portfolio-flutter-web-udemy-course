994622b8370eb0f8ca560a41eb71c3cc6b2efae6
Haris <muhamad_haris@hotmail.com>
2020-05-22 14:25:39 +0800

    Add drawer view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio_flutter_web/header/header_view.dart';
+import 'package:portfolio_flutter_web/navigation_bar/navigation_bar_view.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Happy Haris',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -29,14 +32,57 @@ class PortfolioView extends StatelessWidget {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
+      endDrawer: DrawerView(),
       body: SingleChildScrollView(
         child: Column(
           children: [
+            NavigationBarView(),
             HeaderView(),
             Container(height: height, width: width, color: Colors.blue),
           ],
         ),
       ),
+    );
+  }
+}
+
+class DrawerView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveBuilder(
+      builder: (_, size) {
+        if (!size.isMobile) return SizedBox();
+        return Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('<Insert Your Name>'),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.white, Colors.blue],
+                    tileMode: TileMode.repeated,
+                  ),
+                ),
+              ),
+              for (var item in kNavigationItems)
+                ListTile(
+                  title: Text(item.text),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
